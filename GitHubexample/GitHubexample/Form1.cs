1a990282fcdf26a0a7dfe28acb202ec0018b6353
Ren1055 <47786673+Ren1055@users.noreply.github.com>
2019-05-14 09:42:13 -0500

    Agregué el button2

--- GitHubexample/GitHubexample/Form1.cs
+++ GitHubexample/GitHubexample/Form1.cs
@@ -16,5 +16,10 @@ namespace GitHubexample
         {
             InitializeComponent();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
